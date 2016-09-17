ac802faf484f97a878ab72e89575cda1e8376a07
LoicNyssen <loic@nyssen.com.au>
2016-09-17 12:55:31 +0200

    Added more test to low level BookDAOTest

--- test/libary/LowLevelIntegration/BookDAOTest.java
+++ test/libary/LowLevelIntegration/BookDAOTest.java
@@ -14,43 +14,95 @@ import library.interfaces.entities.IBook;
 
 public class BookDAOTest {
     
-    int noBooks_ = 5;
+    private int noAuthors_ = 4;
+    private int noBooks_   = 5;
     
-    private IBookHelper _BookHelper;
-    private IBook books_;
+    private int totalBooks_ = noAuthors_ * noBooks_;
     
-    BookMapDAO bookMapDAO_;
+    private IBookHelper bookHelper_;
+    
+    private BookMapDAO bookMapDAO_;
+    
+    private String author_[] = new String[totalBooks_];
+    private String title_[] = new String[totalBooks_];
+    private String callNumber_[] = new String[totalBooks_];
     
 
     @Before
     public void setUp() throws Exception {
-        _BookHelper = new BookHelper();
-        bookMapDAO_ = new BookMapDAO(_BookHelper);
+        setUpTestData();
     }
-
-    // I was going to have this as 2 separate tests, but
-    // I can't test list books with out adding books. So I 
-    // Couldn't see the point of testing add book on its own. 
-    // Prioritizing :)
-    @Test
-    public void testAddAndListBooks() {
-        for (int i = 0; i < noBooks_ ; i++) {
-            //arrange
-            String author     = "Author " + i;
-            String title      = "Title " + i;
-            String callNumber = Integer.toString(i);
-            
-            //execute
-            books_ = bookMapDAO_.addBook(author, title, callNumber);
-            
-            //assert
-            assertEquals(author, books_.getAuthor());
-            assertEquals(title, books_.getTitle());
-            assertEquals(callNumber, books_.getCallNumber());
-        }
+    
+    public void setUpTestData() {
+        bookHelper_ = new BookHelper();
+        bookMapDAO_ = new BookMapDAO(bookHelper_);
         
-        List<IBook> bookList = bookMapDAO_.listBooks();
-        assertEquals(noBooks_, bookList.size());
+        int i = 0;
+        
+        for (int a = 0; a < noAuthors_ ; a++) {
+            for (int b = 0; b < noBooks_ ; b++) {
+                author_[i]  = "Author " + a;
+                title_[i]   = "Title " + b;
+                callNumber_[i] = Integer.toString(i);
+                
+                bookMapDAO_.addBook(author_[i], title_[i], callNumber_[i]);
+                
+                i++;
+            }
+        }
     }
 
+    @Test
+    public void testListBooks() {
+        List<IBook> bookList = bookMapDAO_.listBooks();
+
+        assertEquals(totalBooks_, bookList.size());
+    }
+    
+    @Test
+    public void testFindBooksByAuthor() {
+        //arrange
+        String author = "Author 1";
+        
+        //execute
+        List<IBook> books = bookMapDAO_.findBooksByAuthor(author);
+        for (IBook book : books) {
+            String actual = book.getAuthor();
+        
+            //assert
+            assertEquals (author, actual);
+        }
+        assertEquals(noBooks_, books.size());
+    }
+
+    @Test
+    public void testFindBooksByTitle() {
+        //arrange
+        String title = "Title 1";
+        
+        //execute
+        List<IBook> books = bookMapDAO_.findBooksByTitle(title);
+        for (IBook book : books) {
+            String actual = book.getTitle();
+        
+            //assert
+            assertEquals (title, actual);
+        }
+        assertEquals(noAuthors_, books.size());
+    }
+    
+    @Test
+    public void testFindBooksByAuthorTitle() {
+        //arrange
+        String author = "Author 1";
+        String title  = "Title 1";
+        
+        int noBooksExpected = 1;
+        
+        //execute
+        List<IBook> books = bookMapDAO_.findBooksByAuthorTitle(author, title);
+        
+        //assert
+        assertEquals(noBooksExpected, books.size());
+    }
 }
